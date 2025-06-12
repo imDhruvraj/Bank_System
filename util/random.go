61f515abea77f78321ced8b2e7d2ff444b89ee4a
imDhruvraj <dhruvrajsinh.vaghela79@gmail.com>
2025-06-12 09:40:21 +0530

    update

--- util/random.go
+++ util/random.go
@@ -41,7 +41,7 @@ func RandomMoney() int64 {
 
 //RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "INR"}
+	currencies := []string{EUR, USD, INR}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
